bf34c6e2a90c4437ddcf563fe0610b823b0456b4
Jonas Kello <jonas.kello@gmail.com>
2022-10-19 22:27:13 +0200

    Update with new issue

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,11 @@
-function testone(x: readonly string[]): string {
-  return "";
-}
-
-interface Foo {
-  readonly bar: string;
-}
-
-function testtwo(quantity: string): ReadonlyArray<Foo> {
-  return [{ bar: "" }];
-}
-
-export type UnitSegment =
-  | ((SingleSegment | DoubleSegment) & { readonly length: number })
-  | (PairedSegment & {
-      readonly inlet: HalfSegment & { readonly length: number };
-      readonly outlet: HalfSegment & { readonly length: number };
-    });
-
-export type SingleSegment = {
-  readonly type: "Single";
-  //   readonly boxes: ReadonlyArray<BoxDef>;
-  //   readonly ori: OriType;
-};
-
 export type DoubleSegment = {
   readonly type: "Double";
-  //   readonly boxes: ReadonlyArray<BoxDef>;
-  //   readonly ori: OriType;
 };
 
-export type PairedSegment = {
-  readonly type: "Paired";
-  readonly inlet: HalfSegment;
-  readonly outlet: HalfSegment;
-  //   readonly ori: OriType;
+export type BoxDef = {
+  readonly type: "BoxDef";
 };
 
-export type HalfSegment = {
-  //   readonly boxes: ReadonlyArray<BoxDef>;
-  readonly part: HalfSegmentPart;
-  readonly sameOri: boolean;
-};
-export type HalfSegmentPart = "lower" | "upper" | "front" | "back";
-
-function pairSingleSegments(): ReadonlyArray<UnitSegment> {
-  return [];
-}
+export const doubleSegment = (boxes: BoxDef): DoubleSegment => ({
+  type: "Double",
+});
